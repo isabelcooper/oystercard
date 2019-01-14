b915a6641e549e745209d3fbb513e4012815164b
Katie Cornish <katielcornish@gmail.com>
2019-01-14 15:59:27 +0000

    added methods to wheck whether or not a card is in use or not

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,11 +6,28 @@ class Oystercard
   def initialize
     @balance = 0
     @limit = LIMIT
+    @in_journey = false
   end
 
   def top_up(value)
     fail "This exceeds the £#{limit} limit" if limit_exceeded?(value)
     @balance += value
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,4 +15,24 @@ describe Oystercard do
     expect{ subject.top_up(subject.limit + 1) }.to raise_error "This exceeds the £#{subject.limit} limit"
   end
 
+  it "should deduct a fare from the card balance when the deduct menthod is called" do
+    subject.top_up(10)
+    subject.deduct(2)
+    expect(subject.balance).to eq 8
+  end
+
+  it "will have an in_journey? status of false when a card is initialized" do
+    expect(subject.in_journey?).to eq false
+  end
+
+  it "will change the status of @in_journey to true when the touch_in method is called" do
+    subject.touch_in
+    expect(subject.in_journey?).to eq true
+  end
+
+  it "will change the status of @in_journey to false when the touch_out method is called" do
+    subject.touch_out
+    expect(subject.in_journey?).to eq false
+  end
+
 end
