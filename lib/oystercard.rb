f974bd082acd96e5d5b9b471124b29910e30171e
Katie Cornish <katielcornish@gmail.com>
2019-01-14 15:10:59 +0000

    added a method to check whether topping up will exceed the card balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,20 @@
 class Oystercard
+
   attr_reader :balance
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(value)
+    fail "This exceeds the £90 limit" if limit_exceeded?(value)
+    @balance += value
+  end
+
+  private
+
+  def limit_exceeded?(value)
+    balance + value > 90
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,18 @@
 require 'oystercard'
 
 describe Oystercard do
-  it { is_expected.to respond_to(:balance) }
+
+  it "has a 0 balance when a new card is initialized" do
+    expect(subject.balance).to eq 0
+  end
+
+  it "amends the card balance when the top up method is called" do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
+
+  it "will raise an error if the user tries to top up the card beyond a limit" do
+    expect{ subject.top_up(100) }.to raise_error "This exceeds the £90 limit"
+  end
+
 end
