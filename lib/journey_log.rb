497d605439d7a04d577d9787c4c0de8e324c7aac
Sebastian Lavender <sebastianlavender@outlook.com>
2019-01-16 15:45:13 +0000

    journey log using doubles to test start of journey and reset

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -2,15 +2,34 @@ require_relative 'journey'
 
 class JourneyLog
 
-  attr_reader :journey
+  attr_reader :single_journey, :journeys
 
-  def initialize(journey = Journey.new)
-    @journey = journey
-    @log = []
+  def initialize(single_journey = Journey)
+    @single_journey = single_journey.new
+    @journeys = []
+  end
+
+  def start_journey(station)
+    start_station(station)
+    store
+    reset
   end
 
   def start_station(station)
-    journey.update_entry_station(station)
+    @single_journey.update_entry_station(station)
+  end
+
+  def exit_station(station)
+    @single_journey.update_exit_station(station)
+  end
+
+  private
+  def store
+    @journeys << single_journey
+  end
+
+  def reset
+    @single_journey = Journey.new
   end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,12 +1,31 @@
 require 'journey_log'
-require 'journey'
 
 describe JourneyLog do
   let(:station) { double(:station) }
+  let(:single_journey) { double(:journey, update_entry_station: station, update_exit_station: station) }
+  let(:journey_class) { double(:single_journey, new: single_journey) }
+  let(:journey_log) { JourneyLog.new(journey_class) }
 
-  it 'should store the start of a new journey' do
-    subject.start_station(station)
-    expect(subject.journey.entry_station).to eq(station)
+  describe '#start_journey' do
+    it 'should store the start of a new journey' do
+      expect(single_journey).to receive(:update_entry_station)
+      journey_log.start_station(station)
+    end
+
+    it 'should store a journey in journeys' do
+      journey_log.start_journey(station)
+      expect(journey_log.journeys).to include(single_journey)
+    end
+
+    it 'should reset single_journey' do
+      journey_log.start_journey(station)
+      expect(journey_log.single_journey).to_not equal(single_journey)
+    end
+  end
+
+  it 'should store the end of a new journey' do
+    expect(single_journey).to receive(:update_exit_station)
+    journey_log.exit_station(station)
   end
 
 end
